fcd4f8191bd5920cb594d646884c9b4384d12688
Daniel Ukaji <dannyukaji@gmail.com>
2023-09-20 15:48:59 +0100

    New Commit

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     <AuthProvider>
       <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
